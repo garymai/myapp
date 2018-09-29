fc759ac5753ee7bdb670ac1393dfc4b7b38b2692
Gary Mai <garymai@Garys-MacBook-Pro.local>
2018-09-29 15:26:46 -0700

    initial commit

---
+++ app/src/main/java/com/example/garymai/myapplication/ui/mainactivity2/MainActivity2ViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.garymai.myapplication.ui.mainactivity2
+
+import android.arch.lifecycle.ViewModel
+
+class MainActivity2ViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
